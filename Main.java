cc38603a4e678f65045eb8b48b4f59978cff63c1
dongkyu <dongkyu.parkk@gmail.com>
2022-01-13 17:36:28 +0900

    feat: Add triangle area calculator

--- Main.java
+++ Main.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         InputManager inputManager = new InputManager();
+        OutputManager ouputManager = new OutputManager();
         ArrayList<Point> pointList = new ArrayList<>();
         Calculator calculator = new Calculator();
 
-        pointList = inputManager.convertInputToPointList();
-        Point pointA = pointList.get(0);
-        Point pointB = pointList.get(1);
-        calculator.distancePointToPoint(pointA, pointB);
+        pointList = inputManager.convertInputToPointList(); // input값 변환 + 출력
+
+        if (pointList.size() == 2) {
+            Point pointA = pointList.get(0);
+            Point pointB = pointList.get(1);
+            return;
+        }
+
+        double area = calculator.TriangleArea(pointList);
+        ouputManager.printTriangleArea(area); // 출력
     }
 }
--- OuputManager.java
+++
@@ -1,7 +0,0 @@
-public class OuputManager {
-    private static final String MESSAGE_TWO_POINT_DISTANCE= "두 점 사이의 거리는 ";
-
-    public void printDistance(double distance) {
-        System.out.println(MESSAGE_TWO_POINT_DISTANCE + distance);
-    }
-}
